c3abc625cbd0b89eb91b2d54420b57f3c904477a
tach2004 <chris-ack@web.de>
2026-07-05 22:22:31 +0200

    Update dbus-shelly-3em-smartmeter.py

--- dbus-shelly-3em-smartmeter.py
+++ dbus-shelly-3em-smartmeter.py
@@ -88,7 +88,7 @@ class DbusShelly3emService:
     self._lastUpdate = 0
  
     # add _update function 'timer'
-    gobject.timeout_add(2000, self._update) # pause 1000ms before the next request
+    gobject.timeout_add(500, self._update) # pause 500ms before the next request
     
     # add _signOfLife 'timer' to get feedback in log every 5minutes
     gobject.timeout_add(self._getSignOfLifeInterval()*60*1000, self._signOfLife)
@@ -222,9 +222,9 @@ class DbusShelly3emService:
        # New Version - from xris99
        #Calc = 60min * 60 sec / 0.500 (refresh interval of 500ms) * 1000
        if (self._dbusservice['/Ac/Power'] > 0):
-            self._dbusservice['/Ac/Energy/Forward'] = self._dbusservice['/Ac/Energy/Forward'] + (self._dbusservice['/Ac/Power']/(60*60/2.0*1000))            
+            self._dbusservice['/Ac/Energy/Forward'] = self._dbusservice['/Ac/Energy/Forward'] + (self._dbusservice['/Ac/Power']/(60*60/0.5*1000))            
        if (self._dbusservice['/Ac/Power'] < 0):
-            self._dbusservice['/Ac/Energy/Reverse'] = self._dbusservice['/Ac/Energy/Reverse'] + (self._dbusservice['/Ac/Power']*-1/(60*60/2.0*1000))
+            self._dbusservice['/Ac/Energy/Reverse'] = self._dbusservice['/Ac/Energy/Reverse'] + (self._dbusservice['/Ac/Power']*-1/(60*60/0.5*1000))
 
        
        #logging
